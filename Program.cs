6f14ece1ab0d3a9edb77a050a9a4068651c4ab0e
Andre Felipe <andrefelipe.pi2@gmail.com>
2023-02-16 16:51:17 -0300

    Estrutura sequencial

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Globalization;
+
+namespace Area_Circulo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Clear();
+            Console.Write("Digite o  raio do circulo: ");
+            double raio = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            double area = 3.14159 * Math.Pow(raio, 2.0);
+            Console.WriteLine("Area = " + area.ToString("F4", CultureInfo.InvariantCulture));
+        }
+    }
+}
